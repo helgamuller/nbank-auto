1ce2318b8890cc5b67b4cc81adf52a70f35c48e1
helgamuller <melnikova.olga@gmail.com>
2025-05-13 16:35:43 -0400

    nbank-iteration2 - junior version API automation aka spagetti-code

---
+++ src/test/java/iteration2/DepositMoneyOnAccountTest.java
@@ -0,0 +1,312 @@
+package iteration2;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+
+public class DepositMoneyOnAccountTest {
+    @BeforeAll
+    public static void setupRestAssured() {
+        RestAssured.filters(
+                List.of(new RequestLoggingFilter(),
+                        new ResponseLoggingFilter(),
+                        new ErrorLoggingFilter()));
+    }
+
+    public static Stream<Arguments> validDepositData() {
+        return Stream.of(
+                Arguments.of(0.01),
+                Arguments.of(4999.99),
+                Arguments.of(5000.00),
+                Arguments.of(100.00)
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("validDepositData")
+    public void userCanDepositValidAmountTest(Double amount) {
+        //create user by admin
+        String username = "aaa" + UUID.randomUUID().toString().replaceAll("[^a-zA-Z0-9]", "").substring(0, 8);
+
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123",
+                           "role": "USER"
+                        }
+                        """, username))
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED);
+
+//get Token/login
+        String userAuthHeader = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123"
+                        
+                        }
+                        """, username))
+                .post("http://localhost:4111/api/v1/auth/login")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .header("Authorization");
+
+        //create an account
+        Integer account = given()
+                .header("Authorization", userAuthHeader)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .post("http://localhost:4111/api/v1/accounts")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED)
+                .extract()
+                .path("id");
+
+        //make a deposit
+        given()
+                .header("Authorization", userAuthHeader)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(String.format("""
+                        {
+                            "id": %d,
+                            "balance": %.2f
+                        }
+                        """, account, amount))
+                .post("http://localhost:4111/api/v1/accounts/deposit")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+        //check if deposit contains provided value
+
+        Response response = given()
+                .header("Authorization", userAuthHeader)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .get(String.format("http://localhost:4111/api/v1/accounts/%s/transactions", account))
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .response();
+        //.body("relatedAccountId", Matchers.equalTo(account))
+
+        //parse response to list
+        List<Map<String, Object>> transactions = response.jsonPath().getList("$");
+
+        boolean isDepositFound = transactions.stream()
+                .anyMatch(transaction -> "DEPOSIT".equals(transaction.get("type")) &&
+                        Double.parseDouble(transaction.get("amount").toString()) == amount);
+
+        Assertions.assertTrue(isDepositFound);
+
+
+    }
+
+    public static Stream<Arguments> invalidDepositData() {
+        return Stream.of(
+                Arguments.of(0.0),
+                Arguments.of(-0.01),
+                Arguments.of(5000.01)
+                //Arguments.of("string")
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidDepositData")
+    public void userCanNotDepositInvalidAmountTest(Double amount) {
+        //create user by admin
+        String username = "aaa" + UUID.randomUUID().toString().replaceAll("[^a-zA-Z0-9]", "").substring(0, 8);
+
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123",
+                           "role": "USER"
+                        }
+                        """, username))
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED);
+
+//get Token/login
+        String userAuthHeader = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123"
+                        
+                        }
+                        """, username))
+                .post("http://localhost:4111/api/v1/auth/login")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .header("Authorization");
+
+        //create an account
+        Integer account = given()
+                .header("Authorization", userAuthHeader)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .post("http://localhost:4111/api/v1/accounts")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED)
+                .extract()
+                .path("id");
+
+        //make a deposit
+        given()
+                .header("Authorization", userAuthHeader)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(String.format("""
+                        {
+                            "id": %d,
+                            "balance": %.2f
+                        }
+                        """, account, amount))
+                .post("http://localhost:4111/api/v1/accounts/deposit")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST);
+        //check if deposit contains provided value
+
+
+
+    }
+    @Test
+    public void unauthorizedUserCanNotMakeDepositTest() {
+        //create user by admin
+        String username = "aaa" + UUID.randomUUID().toString().replaceAll("[^a-zA-Z0-9]", "").substring(0, 8);
+
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123",
+                           "role": "USER"
+                        }
+                        """, username))
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED);
+
+        //get Token
+
+        //make  a deposit
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("""
+                        {
+                            "id": 1,
+                            "balance": 100.00
+                        }
+                        """)
+                .post("http://localhost:4111/api/v1/accounts/deposit")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED);
+
+    }
+    @Test
+    public void userCanNotMakeDepositToAccountWhichNotExistsTest(){
+        //create user by admin
+        String username = "aaa" + UUID.randomUUID().toString().replaceAll("[^a-zA-Z0-9]", "").substring(0, 8);
+
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123",
+                           "role": "USER"
+                        }
+                        """, username))
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED);
+
+//get Token/login
+        String userAuthHeader = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123"
+                        
+                        }
+                        """, username))
+                .post("http://localhost:4111/api/v1/auth/login")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .header("Authorization");
+
+        //make a deposit
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", userAuthHeader)
+                .body("""
+                        {
+                            "id": 150,
+                            "balance": 100.00
+                        }
+                        """)
+                .post("http://localhost:4111/api/v1/accounts/deposit")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_FORBIDDEN);
+    }
+}
---
+++ src/test/java/iteration2/NameChangeTest.java
@@ -0,0 +1,199 @@
+package iteration2;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+
+public class NameChangeTest {
+    @BeforeAll
+    public static void setupRestAssured() {
+        RestAssured.filters(
+                List.of(new RequestLoggingFilter(),
+                        new ResponseLoggingFilter()));
+    }
+
+    @Test
+    public void userCanChangeNameToValidOneTest() {
+        //create user by admin
+        String username = "aaa" + UUID.randomUUID().toString().replaceAll("[^a-zA-Z0-9]", "").substring(0, 8);
+
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123",
+                           "role": "USER"
+                        }
+                        """, username))
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED);
+
+//get Token
+        String userAuthHeader = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123",
+         
+                        }
+                        """, username))
+                .post("http://localhost:4111/api/v1/auth/login")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .header("Authorization");
+        //login by a user
+
+
+        //change a name
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", userAuthHeader)
+                .body("""
+                        {
+                           "name": "olga M"
+                           
+                        }
+                        """)
+                .put("http://localhost:4111/api/v1/customer/profile")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .body("customer.name", Matchers.equalTo("olga M"));
+    }
+    public static Stream<Arguments> invalidNameData(){
+        return Stream.of(
+                Arguments.of(""),
+                Arguments.of("   "),
+                Arguments.of("olga"),
+                Arguments.of("olga olga olga"),
+                Arguments.of("!12$%^& *()")
+                // Arguments.of(null)
+
+
+        );
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidNameData")
+    public void userCanNotChangeNameToInvalidOneTest(String name) {
+        //create user by admin
+        String username = "aaa" + UUID.randomUUID().toString().replaceAll("[^a-zA-Z0-9]", "").substring(0, 8);
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123",
+                           "role": "USER"
+                           }
+                        """, username))
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED);
+
+//get Token
+        String userAuthHeader = given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123",
+                           "role": "USER"
+                           }
+                        """, username))
+                .post("http://localhost:4111/api/v1/auth/login")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK)
+                .extract()
+                .header("Authorization");
+        //login by a user
+
+
+        //change a name
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", userAuthHeader)
+                .body(String.format("""
+            {
+               "name": "%s"
+            }
+            """, name))
+                .put("http://localhost:4111/api/v1/customer/profile")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body("customer.name", Matchers.not(Matchers.equalTo(name)));
+    }
+    @Test
+    public void unauthorizedUserCanNotChangeNameTest() {
+        //create user by admin
+        String username = "aaa" + UUID.randomUUID().toString().replaceAll("[^a-zA-Z0-9]", "").substring(0, 8);
+
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body(String.format("""
+                        {
+                        "username": "%s",
+                           "password": "olga1A$123",
+                           "role": "USER"
+                        }
+                        """, username))
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED);
+
+//get Token
+
+        //change a name
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body("""
+                        {
+                           "name": "olga M"
+                           
+                        }
+                        """)
+                .put("http://localhost:4111/api/v1/customer/profile")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_UNAUTHORIZED);
+
+    }
+}
+
+
