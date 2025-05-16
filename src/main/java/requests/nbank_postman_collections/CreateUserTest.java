58e08e6613fd6b6c602d71b33d9113a2fb727bf7
helgamuller <melnikova.olga@gmail.com>
2025-05-16 11:31:38 -0400

    nbank-iteration2 - junior version API automation aka spagetti-code

---
+++ src/main/java/requests/nbank_postman_collections/CreateUserTest.java
@@ -0,0 +1,94 @@
+package requests.nbank_postman_collections;
+
+import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
+
+public class CreateUserTest {
+    @BeforeAll
+    public static void setupRestAssured(){
+        RestAssured.filters(
+                List.of(new RequestLoggingFilter(),
+                        new ResponseLoggingFilter()));
+    }
+//Username must contain only letters, digits, dashes, underscores, and dots
+    public static Stream<Arguments> userValidData(){
+        return Stream.of(
+                Arguments.of("olgaM1-_.", "Password33$", "USER", "username")
+
+        );
+    }
+    @MethodSource("userValidData")
+    @ParameterizedTest
+    public void adminCanCreateUserWithValidData(String username, String password, String role){
+        String requestBody = String.format(
+                """
+                                               {
+                           "username": "%s",
+                           "password": "%s",
+                           "role": "%s"
+                        }
+                        """, username, password, role);
+
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body(requestBody)
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_CREATED)
+                .body("username", Matchers.equalTo(username))
+                .body("password", Matchers.not(Matchers.equalTo(password)))
+                .body("role", Matchers.equalTo(role));
+
+
+    }
+
+    public static Stream<Arguments> userInvalidData(){
+        return Stream.of(
+                Arguments.of("   ", "Password33$", "USER", "username", "Username cannot be blank"),
+                Arguments.of("ab", "Password33$", "USER", "username", "Username must be between 3 and 15 characters"),
+                Arguments.of("abc$", "Password33$", "USER", "username", "Username must contain only letters, digits, dashes, underscores, and dots"),
+                Arguments.of("abc%", "Password33$", "USER", "username", "Username must contain only letters, digits, dashes, underscores, and dots")
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("userInvalidData")
+    public void adminCanNotCreateUserWithInvalidData(String username, String password, String role, String errorKey, String errorValue){
+        String requestBody = String.format(
+                """
+                                               {
+                           "username": "%s",
+                           "password": "%s",
+                           "role": "%s"
+                        }
+                        """, username, password, role);
+        given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Basic YWRtaW46YWRtaW4=")
+                .body(requestBody)
+                .post("http://localhost:4111/api/v1/admin/users")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_BAD_REQUEST)
+                .body(errorKey, Matchers.equalTo(errorValue));
+
+
+
+    }
+}
